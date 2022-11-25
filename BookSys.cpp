94ac2aacf8f8c594ac454ad49207136bc0f1888e
jabo-bernardo <joelvincent.work@gmail.com>
2022-11-26 01:06:38 +0800

    bug fix: BookSys::updateBook index to update can cause error when removing an element before the element to update.

--- BookSys.cpp
+++ BookSys.cpp
@@ -49,15 +49,17 @@ Book *BookSys::getBook(int bookId) {
 
 void BookSys::updateBook(int bookId, int _bookQuantity) {
     Book *bookToUpdate;
+    int bookToUpdateIndex;
     for (int i = 0; i < books.size(), i++) {
         const Book *currentBook = books.at(i);
         if (currentBook.bookId == bookId) {
           bookToUpdate = currentBook;
+          bookToUpdateIndex = i;
           break;
         }
     }
     bookToUpdate.bookQuantity = _bookQuantity;
-    books.at(bookId) = bookToUpdate;
+    books.at(bookToUpdateIndex) = bookToUpdate;
 }
 
 void BookSys::deleteBook(int bookId) {
