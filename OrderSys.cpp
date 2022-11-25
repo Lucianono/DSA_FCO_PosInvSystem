8c4e4619d9c31d9eb2018a62dd1ad47d9dc9ced2
Suferikusu <vincegahe8282@gmail.com>
2022-11-25 17:00:09 +0800

    OrderDB update #1
    Added the Linked List using struct and added some attributes

--- OrderSys.cpp
+++ OrderSys.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include "OrderSys.h"
 
-using namespace std;
 
 //this file is for POS or (Order/Sale) System
 //tite
 //create a blank database for the orders using linked list
 //should have the following attributes: OrderID, CustomerName, [BooksOrdered, QtyOrder] , UnitPrice, AmountPrice, VAT, DiscountPrice, TotalSales
+
+
+
 // ->>if the customer will pay by cash, add these attribute : Cash, Change
 // ->>if the customer will pay by installment, add these attribute : 1stInstallment, 1stChange, 2ndInstallment, 2ndChange
 //create your own OrderID format
@@ -35,11 +35,112 @@ using namespace std;
 //order should not complete IF QtyOrder > Quantity in Book System
 //every order should decrement the Book quantity
 //create function to display the customer with remaining balance
+
+//void display;
 //create function to display the entire Order Database
 //create function to read or access every attributes given
 
+#include <iostream>
+#include <conio.h>
 
-OrderSys::OrderSys()
+using namespace std;
+
+
+void cashType();
+void installmentType();
+
+struct Osys
 {
+    int OrderID;
+    string CustomerName;
+    int UnitPrice;
+    int AmountPrice;
+    int Vat;
+    int DiscountPrice;
+    int TotalSales;
+    int Cash;
+    int Change;
+    int FirstInstallment;
+    int FirstChange;
+    int SecondFirstInstallment;
+    int SecondChange;
+    Osys *next;
+};
+
+    
+int main()
+{
+
+
+
+    Osys *head;
+    head = NULL;
+
+    Osys *newOsys;
+
+    int orderType;
+    cout << "\n--------------------------------------------------------------\n";
+    cout << "\t-- POS or (Order/Sale) System --\t\t\n";
+    cout << "\t[1] Cash\t[2] Installment\t\n";
+    cout << "--------------------------------------------------------------\n";
+    cout << "\tEnter: "; cin >> orderType;
+
+    switch(orderType)
+    {
+
+    case 1:
+        cashType();
+        break;
+    case 2:
+        installmentType();
+        break;
+
+    }
+
+    newOsys = new Osys;
+    newOsys -> OrderID = 1;
+    newOsys->next=NULL;
+    head=newOsys;
+
+    newOsys = new Osys;
+    newOsys -> OrderID = 2;
+    newOsys->next=NULL;
+    head->next=newOsys;
+    
+    newOsys = new Osys;
+    newOsys -> OrderID = 3;
+    newOsys->next=NULL;
+    head->next=newOsys;
+
+    Osys *displayPointer;
+    displayPointer=head;
+
+    while(displayPointer)
+    {
+
+        cout << displayPointer -> OrderID;
+        displayPointer=displayPointer->next;    
+        getch();
+    }
+
+
     cout<<"OrderSys Class created"<<endl;
+
+
+    getch();
+    return 0;
 }
+
+
+
+void cashType()
+{
+    cout << "\nEnter Cash Amount: ";
+    
+}
+
+void installmentType()
+{
+
+}
+
