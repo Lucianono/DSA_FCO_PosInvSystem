ae8dd962a9b72409fb9a5b1fff4eeb14bcb80b04
Bryyyyy <bryancalulo@gmail.com>
2022-11-26 21:13:38 +0800

    Checked: added comments, need revisions

--- BookSys.cpp
+++ BookSys.cpp
@@ -6,16 +6,15 @@
 //this file is for the CICT Books System
 
 //create a database for the given CICT books using 2D array
-//should have 4 attributes: BookID, BookName, BookYear, Quantity
-//let Quantity of each book be 20
-//BookID will be decided by you, pick which is best
 //the books is in Teams
 
 //create a CRUD system for the Books
-//should access the book by the given Book ID
-//can modify the Quantity
-//should NOT modify the BookID,Name,and Year
-//notify if TotalBookQuantity is <= 5%
+//addBook is not necessary but still good
+//deleteBook is not necessary but still good
+//create a display function to display all books
+//create to display Book Name. (eg. display(0) output: Book1TitleAbc)
+//commenting your code would be a great help
+
 
 //assigned to : Joel-Vincent Bernardo, Jocelyn Rebato, Liah Baculi
 
