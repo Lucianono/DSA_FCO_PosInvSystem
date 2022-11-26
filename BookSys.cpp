578b28797001afe6e264909334fef2a48124bd36
jabo-bernardo <joelvincent.work@gmail.com>
2022-11-26 23:18:02 +0800

    removed unnecessary comments

--- BookSys.cpp
+++ BookSys.cpp
@@ -4,18 +4,6 @@
 #include <vector>
 
 //this file is for the CICT Books System
-
-//create a database for the given CICT books using 2D array
-//the books is in Teams
-
-//create a CRUD system for the Books
-//addBook is not necessary but still good
-//deleteBook is not necessary but still good
-//create a display function to display all books
-//create to display Book Name. (eg. display(0) output: Book1TitleAbc)
-//commenting your code would be a great help
-
-
 //assigned to : Joel-Vincent Bernardo, Jocelyn Rebato, Liah Baculi
 
 int BookSys::currentId = 0;
