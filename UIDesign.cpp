958be81f9bb8ad1366e26266a2bf4fd299228541
Bryyyyy <bryancalulo@gmail.com>
2022-12-03 12:11:15 +0800

    bug fixes

--- UIDesign.cpp
+++ UIDesign.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include "UIDesign.h"
+
+using namespace std;
+
+//this file is responsible for the designs or UI
+//create functions or methods to easily design an output
+//design a receipt
+//design a security UI
+//design an order database UI
+//design an book database UI
+//design an about us UI
+//design a transaction UI
+//be creative!
+//assigned to: Michaela De Guzman
+
+UIDesign::UIDesign()
+{
+    cout<<"UIDesign Class created"<<endl;
+}
 
