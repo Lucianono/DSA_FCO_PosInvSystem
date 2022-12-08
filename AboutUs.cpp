b5d5f8652203f223a3cfc0edd2e95a93fc0d0bee
Bryyyyy <bryancalulo@gmail.com>
2022-12-08 14:32:22 +0800

    functions added

--- AboutUs.cpp
+++ AboutUs.cpp
@@ -13,5 +13,25 @@ using namespace std;
 
 AboutUs::AboutUs()
 {
-    cout<<"AboutUs Class created"<<endl;
+
+}
+
+void AboutUs::displayMembers()
+{
+    //cout here all members' information
+    //please refer guidelines
+}
+
+void AboutUs::displayAssignment()
+{
+    //cout here all members' task given
+    //create a very short intro about how task was distributed and given (by git and github)
+    //LastName and the branch assigned
+    //please refer guidelines
+}
+
+void AboutUs::displayInstructor()
+{
+    //cout here Mam evelyn
+    //please refer guidelines
 }
